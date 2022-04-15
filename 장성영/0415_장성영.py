022a5ebaf7aff6598d94407c6ff2e05633bb88c0
www622846 <www622846@gmail.com>
2022-04-15 17:41:00 +0900

    Python File
    Python File

--- 장성영/0415_장성영.py
+++ 장성영/0415_장성영.py
@@ -23,9 +23,34 @@ while response == "아니":
 print("먹자")"""
 
 #1부터 10까지의 덧셈
-i = 1
+"""i = 1
 sum = 0
 while i <= 10:
     sum += i
     i += 1
-print("1부터 10까지의 합 : %d" % sum)
+print("1부터 10까지의 합 : %d" % sum)"""
+
+#무한 반복문
+"""sign = True
+
+while sign:
+    light = input("신호등의 색을 입력하세요 : ")
+    if(light == "blue"):
+        break
+        '''sign = False'''
+print("전진")"""
+
+#continue (홀수 출력)
+
+"""for i in range(10):
+    if(i % 2 == 0):
+        continue
+    print(i)"""
+
+#구구단 출력
+"""dan = int(input("원하는 단은"))
+i = 1
+while i < 10:
+    print("{0} * {1} = {2}".format(dan, i,dan * i))
+    i += 1
+"""
